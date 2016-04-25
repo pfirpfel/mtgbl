f90e8df91809c8faa5bb8c4f6f79ba85b822c284
Michael Kuenzli <michaelkuenzli@gmail.com>
2016-04-25 17:07:50 +0200

    first step in 'germanization'

--- routes/middleware.js
+++ routes/middleware.js
@@ -24,10 +24,11 @@ exports.initLocals = function(req, res, next) {
 	var locals = res.locals;
 	
 	locals.navLinks = [
-		{ label: 'Home',		key: 'home',		href: '/' },
+		//{ label: 'Home',		key: 'home',		href: '/' },
 		{ label: 'Blog',		key: 'blog',		href: '/blog' },
-		{ label: 'Gallery',		key: 'gallery',		href: '/gallery' },
-		{ label: 'Contact',		key: 'contact',		href: '/contact' }
+		{ label: '\xDCber Uns',		key: 'aboutus',		href: '/aboutus' },
+		{ label: 'Galerie',		key: 'gallery',		href: '/gallery' },
+		{ label: 'Kontakt',		key: 'contact',		href: '/contact' }
 	];
 	
 	locals.user = req.user;
