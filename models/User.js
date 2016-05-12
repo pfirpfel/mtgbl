121fb96422eea655f0aa643f77695f50af54c787
Michael Kuenzli <michaelkuenzli@gmail.com>
2016-05-12 15:04:03 +0200

    fixed reset-password mail text

--- models/User.js
+++ models/User.js
@@ -40,10 +40,11 @@ User.schema.methods.resetPassword = function(callback) {
 			from: process.env.MAILGUN_MAIL || 'MTG Baselland <noreply@mtgbaselland.ch>',
 			to: user.email,
 			subject: 'Zur\xFCcksetzen deines Passworts bei mtgbaselland.ch',
-			text: 'Hallo ' + user.name + '!\n\n' +
+			text: 'Hallo ' + user.name.full + '!\n\n' +
 				'Um dein Passwort zur\xFCckzusetzen, folge bitte diesem Link: ' +
 				'http://mtgbaselland.ch/reset-password/' + user.resetPasswordKey + '\n\n' +
-				'Hast du dein Passwort nicht vergessen, ignoriere diese E-Mail.\n'
+				'Hast du dein Passwort nicht vergessen, ignoriere diese E-Mail.\n\n' +
+				'Gruss,\nMTG Baselland'
 		}, callback);
 	});
 };
